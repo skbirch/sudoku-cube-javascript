4aa98d4e15ce5d9ea8d3b78ee887a89a24445bf1
Samuel Birch <samkbirch@gmail.com>
2022-08-16 15:13:01 -0500

    first viable product

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 // Object Definition //
 function Face() {
   this.id = null;
+  this.color = null;
     this.Matrix = new Array(4).fill().map(()=>Array(4).fill());
     this.PopulateMatrix = function() {
       this.Matrix.forEach(line => line.fill(Array.from(new Array(16), (x, i) => i)));
@@ -22,16 +23,19 @@ function Face() {
 
 function Slab() {
   this.id = null;
+  this.color = null;
   this.Matrix = new Array();
 }
 
 function Slice() {
   this.id = null;
+  this.color = null;
   this.Matrix = new Array();
 }
 
 function Wall() {
   this.id = null;
+  this.color = null;
   this.Matrix = new Array();
 }
 
@@ -170,14 +174,14 @@ function waveformCollapse(cube) {
     }
   }
   // console.log(choiceList);
-  var randomValue = ["id5", [6]]// choiceList.sample();
+  var randomValue = choiceList.sample();
   var foundNumber = randomValue[1].sample();
   var foundFace = searchAndRemoveFromArray(cube.faces, randomValue, foundNumber);
   var foundSlab = searchAndRemoveFromArray(cube.slabs, randomValue, foundNumber);
   var foundSlice = searchAndRemoveFromArray(cube.slices, randomValue, foundNumber);
   var foundWall = searchAndRemoveFromArray(cube.walls, randomValue, foundNumber);
 
-  console.log("Face: " + foundFace + " | Slab: " + foundSlab + " | Slice: " + foundSlice + " | Wall: " + foundWall + " | Value: " + randomValue + " | Number: " + foundNumber);
+  // console.log("Face: " + foundFace + " | Slab: " + foundSlab + " | Slice: " + foundSlice + " | Wall: " + foundWall + " | Value: " + randomValue + " | Number: " + foundNumber);
 
   // reduceSuperPosition(cube, foundFace, foundSlab, foundSlice, foundWall, foundNumber);
   // console.log(randomValue);
@@ -216,7 +220,7 @@ function searchAndRemoveFromArray(arrayList, value, foundNumber) {
         if (arrayList[i].Matrix[j][k][0] == value[0]) {
           // console.log(arrayList[i].Matrix[j][k][0]);
           reduceSuperPosition(arrayList[i], foundNumber);
-          console.log(arrayList[i]);
+          // console.log(arrayList[i]);
           arrayList[i].Matrix[j][k][1] = [foundNumber];// arrayList[i].Matrix[j][k][1].filter(item => item !== foundNumber);
 
           // console.log(arrayList[i].Matrix[j][k][1].filter(item => item !== foundNumber));
@@ -230,7 +234,9 @@ function searchAndRemoveFromArray(arrayList, value, foundNumber) {
 function reduceSuperPosition(array, num) {
   for (var i = 0; i <= 3; i++) {
     for (var j = 0; j <= 3; j++) {
-      array.Matrix[i][j][1] = array.Matrix[i][j][1].filter(item => item !== num);
+      if (array.Matrix[i][j][1].length >1) {
+        array.Matrix[i][j][1] = array.Matrix[i][j][1].filter(item => item !== num);
+      }
     }
   }
 }
@@ -240,17 +246,41 @@ function checkValidity(cube) {
   1. check if any face, slab, slice, or wall has either:
     - blank cell
     - multiple of same number
-
   */
+  if (foundDuplicatesOrBlanks(cube.faces, 6) ||
+      foundDuplicatesOrBlanks(cube.slabs, 4) ||
+      foundDuplicatesOrBlanks(cube.slices, 4) ||
+      foundDuplicatesOrBlanks(cube.walls, 4)) {
+    cube.valid = false;
+  }
 }
 
+function foundDuplicatesOrBlanks(array, count) {
+  for (var i = 0; i <= count - 1; i++) {
+    var singleList = [];
+    for (var j = 0; j <= 3; j++) {
+      for (var k = 0; k <= 3; k++) {
+        if (array[i].Matrix[j][k][1].length == 0) {
+          console.log("NO LENGTH");
+          return true;
+        }
+        if (array[i].Matrix[j][k][1].length == 1) {
+          singleList.push(array[i].Matrix[j][k][1][0]);
+        }
+      }
+    }
+    var noDups = new Set(singleList);
+    // console.log(singleList.length + " | " + noDups);
+    return singleList.length !== noDups.size;
+  }
+}
 
-function displayFaces(face, id, color) {
-  var html = "<table style='table-layout: fixed; width: 350px; background-color:" + color + ";' border='1|1'>";
+function displayFaces(face, id) {
+  var html = "<table style='table-layout: fixed; width: 350px; background-color:" + face.color + ";' border='1|1'>";
   for (var i = 0; i <= 3; i++) {
     html+="<tr>";
     for (var j = 0; j <= 3; j++) {
-    html+="<td style='word-wrap: break-word'>"+displayCellContents(face.Matrix[i][j][1])+"</td>";
+    html+="<td style='word-wrap: break-word'>"+ face.Matrix[i][j][1] +"</td>";
     }
     html+="</tr>";
   }
@@ -258,28 +288,57 @@ function displayFaces(face, id, color) {
 document.getElementById(id).innerHTML = html;
 }
 
-function displayCellContents(cell) {
-  var html = "<table>"
-    for (var i = 0; i <= 3; i++) {
-      html+="<tr>";
-      for (var j = 0; j <= 3; j++) {
-        html+="<td>"+ cell[(i * 4) + j] ??= '' +"</td>";
-      }
-    html+="</tr>";
+function displayArray(face, id) {
+  var html = "<table style='table-layout: fixed; width: 350px;' border='1|1'>";
+  html+="<tr>";
+  for (var i = 0; i <= 3; i++) {
+    for (var j = 0; j <= 3; j++) {
+    html+="<td style='word-wrap: break-word'>"+ face.Matrix[i][j][1] +"</td>";
+    }
   }
+  html+="</tr>";
   html+="</table>";
-
-  return html;
+document.getElementById(id).innerHTML = html;
 }
 
+function setFaceColors(cube) {
+  cube.faces[0].color = "white";
+  cube.faces[1].color = "red";
+  cube.faces[2].color = "yellow";
+  cube.faces[3].color = "orange";
+  cube.faces[4].color = "blue";
+  cube.faces[5].color = "green";
+}
 
 function displayAllFaces(cube) {
-  displayFaces(cube.faces[0], "0", "white");
-  displayFaces(cube.faces[1], "1", "red");
-  displayFaces(cube.faces[2], "2", "yellow");
-  displayFaces(cube.faces[3], "3", "orange");
-  displayFaces(cube.faces[4], "4", "blue");
-  displayFaces(cube.faces[5], "5", "green");
+  displayFaces(cube.faces[0], "0");
+  displayFaces(cube.faces[1], "1");
+  displayFaces(cube.faces[2], "2");
+  displayFaces(cube.faces[3], "3");
+  displayFaces(cube.faces[4], "4");
+  displayFaces(cube.faces[5], "5");
+}
+
+function displayRest(cube) {
+  displayArray(cube.slabs[0], "6");
+  displayArray(cube.slabs[1], "7");
+  displayArray(cube.slabs[2], "8");
+  displayArray(cube.slabs[3], "9");
+
+  displayArray(cube.slices[0], "10");
+  displayArray(cube.slices[1], "11");
+  displayArray(cube.slices[2], "12");
+  displayArray(cube.slices[3], "13");
+
+  displayArray(cube.walls[0], "14");
+  displayArray(cube.walls[1], "15");
+  displayArray(cube.walls[2], "16");
+  displayArray(cube.walls[3], "17");
+}
+
+function displayAll(cube) {
+  displayAllFaces(cube);
+  //displayRest(cube);
 }
 
 
@@ -287,156 +346,3 @@ function displayAllFaces(cube) {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function populateGrid(size, section) {
-  for (var i = 1; i <= (size); i++) {
-      btngroup = "<div class='float_center'><div class='child'><div class='btngroup' id='grid" + i + "'></div></div><div class='clear'></div></div><div class='clear'></div>"
-    document.getElementById(section).insertAdjacentHTML('beforeend', btngroup);
-    for (var r = 1; r <= (size); r++) {
-      var pr = (size * i) - size + r
-      row = "<label><input type='checkbox' class='grid' id='" + pr + "g' value='" + pr + "'/><span class='button'></span></label>"
-      document.getElementById("grid"+i).insertAdjacentHTML('beforeend', row);
-    }
-
-  }
-}
-
-function populateDisplay(size, section) {
-  for (var i = 1; i <= (size); i++) {
-    btngroup = "<div class='float_center'><div class='child'><div class='btngroup' id='disp" + i + "'></div></div><div class='clear'></div></div><div class='clear'></div>"
-    document.getElementById(section).insertAdjacentHTML('beforeend', btngroup);
-    for (var r = 1; r <= (size); r++) {
-      var pr = (size * i) - size + r
-      row = "<label><input type='checkbox'  class='disp' id='" + pr + "d' value='" + pr + "' disabled/><span class='display'></span></label>"
-      document.getElementById("disp"+i).insertAdjacentHTML('beforeend', row);
-    }
-
-  }
-}
-
-function getCheckedGrid() {
-  var array = []
-  var checkboxes = document.querySelectorAll('input.grid[type=checkbox]:checked');
-  for (i = 0; i < checkboxes.length; i++) {
-    array.push(Number(checkboxes[i].value));
-  }
-//  console.log("getCheckedGrid V");
-//  console.log(array.sort())
-  return array.sort()
-}
-
-function getCheckedDisplay() {
-  var array = []
-  var checkboxes = document.querySelectorAll('input.disp[type=checkbox]:checked');
-  for (i = 0; i < checkboxes.length; i++) {
-    array.push(Number(checkboxes[i].value));
-  }
-//  console.log("getCheckedDisplay V");
-//  console.log(array.sort());
-  return array.sort();
-}
-
-function shuffle(a) {
-  for (let i = a.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [a[i], a[j]] = [a[j], a[i]];
-  }
-  return a;
-}
-
-function randomGrid(size) {
-var array = [];
-  for (var i = 1; i <= (size*size); i++) {
-      document.getElementById(i+"d").checked = false;
-      array.push(i);
-      shuffle(array);
-      slarray = array.slice(Math.floor(Math.random() * ((size * size) + 1)));
-      soarray = slarray.sort();
-  }
-  soarray.forEach(element => document.getElementById(element+"d").checked = true);
-//  console.log("randomGrid V");
-//  console.log(soarray);
-return soarray;
-}
-
-function arraysEqual(a,b) {
-    if (a instanceof Array && b instanceof Array) {
-        if (a.length!=b.length)  // assert same length
-            return false;
-        for(var i=0; i<a.length; i++)  // assert each element equal
-            if (!arraysEqual(a[i],b[i]))
-                return false;
-        return true;
-    } else {
-        return a==b;  // if not both arrays, should be the same
-    }
-}
-
-function sleep(milliseconds) {
-  const date = Date.now();
-  let currentDate = null;
-  do {
-    currentDate = Date.now();
-  } while (currentDate - date < milliseconds);
-}
-
-// function randomLoop(size) {
-//   let i = 0
-//   while (arraysEqual(getCheckedDisplay(), getCheckedGrid()) != true) {
-//     randomGrid(size);
-//     getCheckedDisplay();
-//     getCheckedGrid();
-//     if (arraysEqual(getCheckedDisplay(), getCheckedGrid())) {
-//       document.getElementById('title2').innerText = 'Found a match after ' + i + ' iterations!'
-//       console.log(i)
-//     } else {
-//       sleep(100);
-//       document.getElementById('title2').innerText = i
-//       console.log(i)
-
-//     }
-//   i++;
-//   if (i == 1000000) {break;}
-//   }
-//   if (i == 0) {document.getElementById('title2').innerText = 'Found a match after ' + i + ' iterations!'}
-// }
-
-function randomLoop(size, delay, max) {
-  var i = 0
-  var iID = setInterval(function() {
-    //Found it!
-    if (arraysEqual(getCheckedDisplay(), getCheckedGrid())) {
-      document.getElementById('title2').innerText = 'Found a match after ' + i + ' iterations!'
-      console.log(i);
-      clearInterval(iID);
-    } else {
-      //Still looking...
-      randomGrid(size);
-      document.getElementById('title2').innerText = i;
-      console.log(i);
-    }
-    if (++i >= max) {
-      document.getElementById('title2').innerText = 'Hit the ' + max + ' iteration limit... Try again!'
-      clearInterval(iID);
-    }
-  },
-    delay);
-  }
